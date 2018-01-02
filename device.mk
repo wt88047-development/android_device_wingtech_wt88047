fe10f1755e9a5747f2ea8905012c557500c6a226
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-02 16:05:51 -0500

    wt88047: Set IMS to use CodeAurora extension
    Change-Id: I64cba319986748e4dad206ca664ccda7eefb348a

--- device.mk
+++ device.mk
@@ -117,6 +117,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/vendor/etc/thermal-engine.conf
